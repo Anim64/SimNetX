39ff5cfb18c5cc301c84312d2aeddcac718babaa
Tomas Anlauf <tomas.anlauf@seznam.cz>
2023-08-14 15:55:08 +0200

    Started working on a different way to display node details to a user.
    - A panel was added to show neighbors of a selected node and better navigate through the graph

--- MultiVariateNetworkExplorer/MultiVariateNetworkExplorer2/wwwroot/js/nodes.js
+++ MultiVariateNetworkExplorer/MultiVariateNetworkExplorer2/wwwroot/js/nodes.js
@@ -37,6 +37,7 @@
 const nodeHeadingClick = function (event, nodeId, nodeIndex) {
     selectNode(event, nodeId);
     toggleNodeDetails(nodeId, nodeIndex);
+    showNodeNeighbors(nodeId);
 }
 
 const nodeHeadingMouseOver = function (nodeId, opacity) {
@@ -93,6 +94,52 @@ const toggleNodeDetails = function(nodeId, nodeIndex) {
 const closeNodeDetails = function (nodeDetailsId) {
     const nodeDetailsDiv = document.getElementById(nodeDetailsId);
     nodeDetailsDiv.style.display = "none";
+}
+
+const showNodeNeighbors = function (nodeId) {
+    const neighborsContainer = document.getElementById("node-neighbors-section");
+
+    const neighborsHeadlineElement = neighborsContainer.firstElementChild;
+    neighborsHeadlineElement.innerHTML = "Node \"" + nodeId + "\" neighbors";
+
+    createNeighborHeadings(nodeId);
+    
+}
+
+const createNeighborHeadings = function (nodeId) {
+    const neighborsGridElement = d3.select("#neighbors-nav");
+    neighborsGridElement.html("");
+
+    for (const link of graph.links) {
+        let neighbor = null;
+        if (link.source.id === nodeId) {
+            neighbor = link.target;
+        }
+        if (link.target.id === nodeId) {
+            neighbor = link.source;
+        }
+
+        if (neighbor != null) {
+            const { id: neighborId, index: neighborIndex } = neighbor;
+            createNeighborHeading(neighborsGridElement, neighborId, neighborIndex);
+        }
+        
+    }
+
+
+
+}
+
+const createNeighborHeading = function (neighborsGridElement, neighborId, neighborIndex) {
+    neighborsGridElement.append("div")
+        .classed("node-heading", true)
+        .attr("role", "tab")
+        .attr("id", "neighbor-heading-" + neighborId)
+        .on("mouseout", function () { /*mouseOut()*/ })
+        .on("click", function () { nodeHeadingClick(event, neighborId, neighborIndex) })
+        .on("mouseover", function () { /*nodeHeadingMouseOver(id, .2)*/ })
+        .append("h5")
+        .html(neighborId);
 }
 
 
